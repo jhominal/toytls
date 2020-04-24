7eb825561b557e785224edd103da4397f78d4b88
Jean Hominal <jhominal@gmail.com>
2020-04-24 10:44:01 +0200

    refactor: Refactor signature_algorithms.SignatureScheme.

--- toy_tls/content/extensions/signature_algorithms.py
+++ toy_tls/content/extensions/signature_algorithms.py
@@ -1,8 +1,7 @@
 from __future__ import generator_stop
 
-from abc import ABCMeta, abstractmethod
-from functools import partial
-from typing import Sequence
+from abc import abstractmethod
+from typing import Sequence, ClassVar, Type, Generic, TypeVar, Optional
 
 from attr import attrs, attrib
 from attr.validators import instance_of
@@ -13,7 +12,6 @@ from cryptography.hazmat.primitives.asymmetric.ed448 import Ed448PrivateKey, Ed4
 from cryptography.hazmat.primitives.asymmetric.padding import PKCS1v15
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
 from cryptography.hazmat.primitives.hashes import SHA1, SHA224, SHA256, SHA384, SHA512, HashAlgorithm
-from typing_extensions import Protocol
 
 from toy_tls._data_reader import DataReader
 from toy_tls._data_writer import DataWriter
@@ -21,146 +19,132 @@ from toy_tls.content.extensions import ExtensionData
 from toy_tls.enum_with_data import EnumUInt16WithData, ExtensibleEnum
 
 
-class Signer(metaclass=ABCMeta):
+TPublicKey = TypeVar('TPublicKey')
+TPrivateKey = TypeVar('TPrivateKey')
+
+
+class SignatureFunctions(Generic[TPublicKey, TPrivateKey]):
     __slots__ = ()
 
+    public_key_type: ClassVar[Type[TPublicKey]]
+    private_key_type: ClassVar[Type[TPrivateKey]]
+
+    def verify(self, public_key: TPublicKey, signature: bytes, data: bytes):
+        if not isinstance(public_key, self.public_key_type):
+            raise ValueError(
+                f'Expected private_key of type {self.public_key_type.__name__} but got {type(public_key).__name__}'
+            )
+        return self._verify(public_key=public_key, signature=signature, data=data)
+
+    def sign(self, private_key: TPrivateKey, data: bytes) -> bytes:
+        if not isinstance(private_key, self.private_key_type):
+            raise ValueError(
+                f'Expected private_key of type {self.private_key_type.__name__} but got {type(private_key).__name__}'
+            )
+        return self._sign(private_key=private_key, data=data)
+
     @abstractmethod
-    def sign(self, data: bytes) -> bytes:
-        raise NotImplementedError()
-
-
-class SignerFactory(Protocol):
-    def __call__(self, private_key) -> Signer:
-        raise NotImplementedError()
-
-
-class Verifier(metaclass=ABCMeta):
-    __slots__ = ()
+    def _verify(self, public_key: TPublicKey, signature: bytes, data: bytes):
+        raise NotImplementedError
 
     @abstractmethod
-    def verify(self, signature: bytes, data: bytes):
-        raise NotImplementedError()
-
-
-class VerifierFactory(Protocol):
-    def __call__(self, public_key) -> Verifier:
-        raise NotImplementedError()
+    def _sign(self, private_key: TPrivateKey, data: bytes) -> bytes:
+        raise NotImplementedError
 
 
 @attrs(auto_attribs=True, frozen=True)
-class RSASigner(Signer):
-    private_key: RSAPrivateKey = attrib(validator=instance_of(RSAPrivateKey))
+class RSASignatureFunctions(SignatureFunctions[RSAPublicKey, RSAPrivateKey]):
     hash: HashAlgorithm = attrib(validator=instance_of(HashAlgorithm))
 
-    def sign(self, data: bytes) -> bytes:
-        return self.private_key.sign(data=data, padding=PKCS1v15(), algorithm=self.hash)
+    public_key_type = RSAPublicKey
+    private_key_type = RSAPrivateKey
+
+    def _verify(self, public_key: RSAPublicKey, signature: bytes, data: bytes):
+        return public_key.verify(signature=signature, data=data, padding=PKCS1v15(), algorithm=self.hash)
+
+    def _sign(self, private_key: RSAPrivateKey, data: bytes) -> bytes:
+        return private_key.sign(data=data, padding=PKCS1v15(), algorithm=self.hash)
 
 
 @attrs(auto_attribs=True, frozen=True)
-class RSAVerifier(Verifier):
-    public_key: RSAPublicKey = attrib(validator=instance_of(RSAPublicKey))
+class DSSSignatureFunctions(SignatureFunctions[DSAPublicKey, DSAPrivateKey]):
     hash: HashAlgorithm = attrib(validator=instance_of(HashAlgorithm))
 
-    def verify(self, signature: bytes, data: bytes):
-        return self.public_key.verify(signature=signature, data=data, padding=PKCS1v15(), algorithm=self.hash)
+    public_key_type = DSAPublicKey
+    private_key_type = DSAPrivateKey
+
+    def _verify(self, public_key: DSAPublicKey, signature: bytes, data: bytes):
+        return public_key.verify(signature=signature, data=data, algorithm=self.hash)
+
+    def _sign(self, private_key: DSAPrivateKey, data: bytes) -> bytes:
+        return private_key.sign(data=data, algorithm=self.hash)
 
 
 @attrs(auto_attribs=True, frozen=True)
-class DSSSigner(Signer):
-    private_key: DSAPrivateKey = attrib(validator=instance_of(DSAPrivateKey))
+class ECDSASignatureFunctions(SignatureFunctions[EllipticCurvePublicKey, EllipticCurvePrivateKey]):
     hash: HashAlgorithm = attrib(validator=instance_of(HashAlgorithm))
 
-    def sign(self, data: bytes) -> bytes:
-        return self.private_key.sign(data=data, algorithm=self.hash)
+    public_key_type = EllipticCurvePublicKey
+    private_key_type = EllipticCurvePrivateKey
+
+    def _verify(self, public_key: EllipticCurvePublicKey, signature: bytes, data: bytes):
+        return public_key.verify(signature=signature, data=data, signature_algorithm=ECDSA(self.hash))
+
+    def _sign(self, private_key: EllipticCurvePrivateKey, data: bytes) -> bytes:
+        return private_key.sign(data=data, signature_algorithm=ECDSA(self.hash))
 
 
-@attrs(auto_attribs=True, frozen=True)
-class DSSVerifier(Verifier):
-    public_key: DSAPublicKey = attrib(validator=instance_of(DSAPublicKey))
-    hash: HashAlgorithm = attrib(validator=instance_of(HashAlgorithm))
+class Ed25519SignatureFunctions(SignatureFunctions[Ed25519PublicKey, Ed25519PrivateKey]):
+    public_key_type = Ed25519PublicKey
+    private_key_type = Ed25519PrivateKey
 
-    def verify(self, signature: bytes, data: bytes):
-        return self.public_key.verify(signature=signature, data=data, algorithm=self.hash)
+    def _verify(self, public_key: Ed25519PublicKey, signature: bytes, data: bytes):
+        return public_key.verify(signature=signature, data=data)
 
-
-@attrs(auto_attribs=True, frozen=True)
-class ECDSASigner(Signer):
-    private_key: EllipticCurvePrivateKey = attrib(validator=instance_of(EllipticCurvePrivateKey))
-    hash: HashAlgorithm = attrib(validator=instance_of(HashAlgorithm))
-
-    def sign(self, data: bytes) -> bytes:
-        return self.private_key.sign(data=data, signature_algorithm=ECDSA(self.hash))
+    def _sign(self, private_key: Ed25519PrivateKey, data: bytes) -> bytes:
+        return private_key.sign(data)
 
 
-@attrs(auto_attribs=True, frozen=True)
-class ECDSAVerifier(Verifier):
-    public_key: EllipticCurvePublicKey = attrib(validator=instance_of(EllipticCurvePublicKey))
-    hash: HashAlgorithm = attrib(validator=instance_of(HashAlgorithm))
+class Ed448SignatureFunctions(SignatureFunctions[Ed448PublicKey, Ed448PrivateKey]):
+    public_key_type = Ed448PublicKey
+    private_key_type = Ed448PrivateKey
 
-    def verify(self, signature: bytes, data: bytes):
-        return self.public_key.verify(signature=signature, data=data, signature_algorithm=ECDSA(self.hash))
+    def _verify(self, public_key: Ed448PublicKey, signature: bytes, data: bytes):
+        return public_key.verify(signature=signature, data=data)
 
-
-@attrs(auto_attribs=True, frozen=True)
-class Ed25519Signer(Signer):
-    private_key: Ed25519PrivateKey = attrib(validator=instance_of(Ed25519PrivateKey))
-
-    def sign(self, data: bytes) -> bytes:
-        return self.private_key.sign(data)
-
-
-@attrs(auto_attribs=True, frozen=True)
-class Ed25519Verifier(Verifier):
-    public_key: Ed25519PublicKey = attrib(validator=instance_of(Ed25519PublicKey))
-
-    def verify(self, signature: bytes, data: bytes):
-        return self.public_key.verify(signature=signature, data=data)
-
-
-@attrs(auto_attribs=True, frozen=True)
-class Ed448Signer(Signer):
-    private_key: Ed448PrivateKey = attrib(validator=instance_of(Ed448PrivateKey))
-
-    def sign(self, data: bytes) -> bytes:
-        return self.private_key.sign(data)
-
-
-@attrs(auto_attribs=True, frozen=True)
-class Ed448Verifier(Verifier):
-    public_key: Ed448PublicKey = attrib(validator=instance_of(Ed448PublicKey))
-
-    def verify(self, signature: bytes, data: bytes):
-        return self.public_key.verify(signature=signature, data=data)
+    def _sign(self, private_key: Ed448PrivateKey, data: bytes) -> bytes:
+        return private_key.sign(data)
 
 
 class SignatureScheme(EnumUInt16WithData, ExtensibleEnum):
-    rsa_pkcs1_sha1 = (0x0201, partial(RSASigner, hash=SHA1()), partial(RSAVerifier, hash=SHA1()))
-    dss_sha1 = (0x0202, partial(DSSSigner, hash=SHA1()), partial(DSSVerifier, hash=SHA1()))
-    ecdsa_sha1 = (0x0203, partial(ECDSASigner, hash=SHA1()), partial(ECDSAVerifier, hash=SHA1()))
-    rsa_pkcs1_sha224 = (0x0301, partial(RSASigner, hash=SHA224()), partial(RSAVerifier, hash=SHA224()))
-    dss_sha224 = (0x0302, partial(DSSSigner, hash=SHA224()), partial(DSSVerifier, hash=SHA224()))
-    ecdsa_sha224 = (0x0303, partial(ECDSASigner, hash=SHA224()), partial(ECDSAVerifier, hash=SHA224()))
-    rsa_pkcs1_sha256 = (0x0401, partial(RSASigner, hash=SHA256()), partial(RSAVerifier, hash=SHA256()))
-    dss_sha256 = (0x0402, partial(DSSSigner, hash=SHA256()), partial(DSSVerifier, hash=SHA256()))
-    ecdsa_sha256 = (0x0403, partial(ECDSASigner, hash=SHA256()), partial(ECDSAVerifier, hash=SHA256()))
-    rsa_pkcs1_sha384 = (0x0501, partial(RSASigner, hash=SHA384()), partial(RSAVerifier, hash=SHA384()))
-    dss_sha384 = (0x0502, partial(DSSSigner, hash=SHA384()), partial(DSSVerifier, hash=SHA384()))
-    ecdsa_sha384 = (0x0503, partial(ECDSASigner, hash=SHA384()), partial(ECDSAVerifier, hash=SHA384()))
-    rsa_pkcs1_sha512 = (0x0601, partial(RSASigner, hash=SHA512()), partial(RSAVerifier, hash=SHA512()))
-    dss_sha512 = (0x0602, partial(DSSSigner, hash=SHA512()), partial(DSSVerifier, hash=SHA512()))
-    ecdsa_sha512 = (0x0603, partial(ECDSASigner, hash=SHA512()), partial(ECDSAVerifier, hash=SHA512()))
-    ed25519 = (0x0807, Ed25519Signer, Ed25519Verifier)
-    ed448 = (0x0808, Ed448Signer, Ed448Verifier)
+    rsa_pkcs1_sha1 = (0x0201, RSASignatureFunctions(hash=SHA1()))
+    dss_sha1 = (0x0202, DSSSignatureFunctions(hash=SHA1()))
+    ecdsa_sha1 = (0x0203, ECDSASignatureFunctions(hash=SHA1()))
+    rsa_pkcs1_sha224 = (0x0301, RSASignatureFunctions(hash=SHA224()))
+    dss_sha224 = (0x0302, DSSSignatureFunctions(hash=SHA224()))
+    ecdsa_sha224 = (0x0303, ECDSASignatureFunctions(hash=SHA224()))
+    rsa_pkcs1_sha256 = (0x0401, RSASignatureFunctions(hash=SHA256()))
+    dss_sha256 = (0x0402, DSSSignatureFunctions(hash=SHA256()))
+    ecdsa_sha256 = (0x0403, ECDSASignatureFunctions(hash=SHA256()))
+    rsa_pkcs1_sha384 = (0x0501, RSASignatureFunctions(hash=SHA384()))
+    dss_sha384 = (0x0502, DSSSignatureFunctions(hash=SHA384()))
+    ecdsa_sha384 = (0x0503, ECDSASignatureFunctions(hash=SHA384()))
+    rsa_pkcs1_sha512 = (0x0601, RSASignatureFunctions(hash=SHA512()))
+    dss_sha512 = (0x0602, DSSSignatureFunctions(hash=SHA512()))
+    ecdsa_sha512 = (0x0603, ECDSASignatureFunctions(hash=SHA512()))
+    ed25519 = (0x0807, Ed25519SignatureFunctions())
+    ed448 = (0x0808, Ed448SignatureFunctions())
 
     def __init__(
         self,
         value: int,
-        signer_factory: SignerFactory = None,
-        verifier_factory: VerifierFactory = None,
+        signature_functions: Optional[SignatureFunctions] = None,
     ):
-        super().__init__(value, signer_factory, verifier_factory)
-        self.signer_factory = signer_factory
-        self.verifier_factory = verifier_factory
+        super().__init__(value, signature_functions)
+        self.signature_functions = signature_functions
+
+    def verify(self, public_key, signature: bytes, data: bytes):
+        return self.signature_functions.verify(public_key=public_key, signature=signature, data=data)
 
 
 @attrs(auto_attribs=True, slots=True)
@@ -183,7 +167,7 @@ class DigitalSignature:
             writer.write_bytes(self.signature)
 
     def verify(self, public_key, data: bytes):
-        self.scheme.verifier_factory(public_key=public_key).verify(signature=self.signature, data=data)
+        self.scheme.verify(public_key=public_key, signature=self.signature, data=data)
 
 
 @attrs(auto_attribs=True, slots=True, frozen=True)
